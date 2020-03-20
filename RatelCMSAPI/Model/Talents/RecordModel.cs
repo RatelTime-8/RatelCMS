427b9626c38f4d6227cee97f9a5b986cd373f51d
王良昊 <@DESKTOP-BR31NAV>
2020-03-20 19:56:17 +0800

    王良昊

---
+++ RatelCMSAPI/Model/Talents/RecordModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Talents
+{
+    public class RecordModel
+    {
+        public int RId { get; set; }
+        public string Organization { get; set; }
+        public string Position { get; set; }
+        public string Pay { get; set; }
+        public string GongNeiR { get; set; }
+        public string LiZhi { get; set; }
+        public DateTime RTime { get; set; }
+    }
+}
---
+++ RatelCMSAPI/Model/Talents/TalentsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Talents
+{
+    public class TalentsModel
+    {
+        public int TId { get; set; }
+        public string TName { get; set; }
+        public string TelePhone { get; set; }
+        public string Graduate { get; set; }
+        public string Education { get; set; }
+        public string Station { get; set; }
+        public string Information { get; set; }
+        public string Email { get; set; }
+        public string FanKui { get; set; }
+        public bool TState { get; set; }
+    }
+}
