1644f166c0269f8e03cca83e249020b7e90e11bc
guan peng <guan peng@DESKTOP-KB5R02M>
2020-03-20 19:18:35 +0800

    报销管理

---
+++ RatelCMSAPI/Model/Reimbursement/Reimbursementtype.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Reimbursement
+{
+    public class Reimbursementtype
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 报销类别名称
+        /// </summary>
+        public string CostTypeName { get; set; }
+    }
+}
