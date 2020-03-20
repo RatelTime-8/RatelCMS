977dd2ceea7a1d06b299555142dc0a686016dbec
ASUS <ASUS@DESKTOP-300DDIH>
2020-03-20 19:17:37 +0800

    添加Project字段

---
+++ RatelCMSAPI/Model/Project/StagePlanInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    //阶段计划表
+    public class StagePlanInfo
+    {
+        //Id
+        public int PlanId { get; set; }
+        //阶段名称
+        public int StageNameId { get; set; }
+        //开始日期
+        public DateTime StageStartTime { get; set; }
+        //完成日期
+        public DateTime StageFinishTime { get; set; }
+        //阶段负责人
+        public int StageStaffId { get; set; }
+    }
+}
