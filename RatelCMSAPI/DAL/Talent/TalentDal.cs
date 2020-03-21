2e2f45433b2341fdaa724a76f32d4ba8f4dbbacf
王良昊 <@DESKTOP-BR31NAV>
2020-03-21 09:52:31 +0800

    wlh

---
+++ RatelCMSAPI/DAL/Talent/TalentDal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using Model;
+using Model.Talents;
+
+namespace DAL
+{
+    public class TalentDal
+    {
+
+    }
+}
