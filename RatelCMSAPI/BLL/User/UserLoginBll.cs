99f9dddb4c933ad98f79ab63ff81b59df179a964
Y-kang <62330449+Y-kang@users.noreply.github.com>
2020-03-23 16:27:55 +0800

    ..

---
+++ RatelCMSAPI/BLL/User/UserLoginBll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Model;
+
+namespace BLL
+{
+   public class UserLoginBll
+    {
+        Usersdal dal = new Usersdal();
+        public UserInfo UserInfoLgoin(string phone, string pass)
+        {
+            return dal.UserInfoLgoin(phone,pass);
+        }
+    }
+}
--- RatelCMSAPI/DAL/Talent/Talentdal.cs
+++ RatelCMSAPI/DAL/Talent/Talentdal.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace DAL.Talent
 {
-    class Talentdal
+   public class Talentdal
     {
+
     }
 }
