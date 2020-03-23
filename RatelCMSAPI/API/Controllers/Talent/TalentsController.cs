08cf3e7804dc7dddfb55a1f4828b2d4eeb13f540
王良昊 <@DESKTOP-BR31NAV>
2020-03-23 15:05:05 +0800

    王良昊

---
+++ RatelCMSAPI/API/Controllers/Talent/TalentsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BLL.Talent;
+using Model.Talents;
+
+namespace API.Controllers.Talent
+{
+    public class TalentsController : ApiController
+    {
+        TalentBll bll = new TalentBll();
+        [HttpPost]
+        public int AddTalent(TalentsModel model)
+        {
+            return bll.AddTalent(model);
+        }
+        [HttpGet]
+        public List<TalentsModel> Show(string Name)
+        {
+            return bll.Show(Name);
+        }
+        /// <summary>
+        /// 修改
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPut]
+        public int PutTalent(TalentsModel model)
+        {
+            return bll.PutTalent(model);
+        }
+    }
+}
---
+++ RatelCMSAPI/DAL/Talent/RecordDal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Talents;
+
+namespace DAL.Talent
+{
+    public class RecordDal
+    {
+        /// <summary>
+        /// 添加雇员信息
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public int AddRecoed(RecordModel model)
+        {
+            string sql = string.Format("insert into Record values('{0}','{1}','{2}','{3}','{4}','{5})", model.Organization, model.Position, model.Pay, model.GongNeiR, model.LiZhi, model.RTime);
+            return DBHelper.ExecuteNonQuery(sql);
+        }
+        /// <summary>
+        /// 显示雇员信息
+        /// </summary>
+        /// <returns></returns>
+        public List<RecordModel> ShowRecoed()
+        {
+            string sql = string.Format("select * from Record");
+            return DBHelper.GetToList<RecordModel>(sql);
+        }
+        public int DeleteRecoed(int Id)
+        {
+            string sql = string.Format("delete from Recoed where RId=" + Id);
+            return DBHelper.ExecuteNonQuery(sql);
+        }
+    }
+}
