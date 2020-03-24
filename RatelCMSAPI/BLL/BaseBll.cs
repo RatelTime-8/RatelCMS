858abf12fb4d2d8ddadcaa4290f5754f7d5b81e0
ASUS <ASUS@DESKTOP-300DDIH>
2020-03-24 14:55:38 +0800

    BLL层基础框架搭建

---
+++ RatelCMSAPI/BLL/BaseBll.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+        /// <summary>
+        /// 单例模式
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        public abstract class BaseBll<T> where T : new()
+        {
+            private static T _instance;
+            public static T Instance
+            {
+                get
+                {
+                    if (_instance == null)
+                        _instance = new T();
+                    return _instance;
+                }
+            }
+        }
+    }
--- RatelCMSAPI/BLL/Project/ProjectBll.cs
+++ RatelCMSAPI/BLL/Project/ProjectBll.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DAL;
+using SDKClient;
 using Model;
 
 namespace BLL
@@ -11,18 +12,136 @@ namespace BLL
     public class ProjectBll
     {
         Projectdal dal = new Projectdal();
+
         /// <summary>
         /// 项目参与人员绑定
         /// 根据所在职位部门
         /// </summary>
-        public List<PositionInfo> BandProjectbinding(string DepartId)
+        public UserGetResponse BandProjectbinding(UserAddRequest request)
         {
-            return dal.BandProjectbinding(DepartId);
+            UserGetResponse response = new UserGetResponse();
+
+            return response;
         }
 
-        public List<ProjectInfo> ProjectShow(int PageIndex, int PageSize, string ProjectNumber, int Projectstage, out int TotalCount)
+        /// <summary>
+        /// 新增项目
+        /// </summary>
+        public ProjectAddResponse AddProject(ProjectAddRequest request)
         {
-            return dal.ProjectShow(PageIndex,PageSize,ProjectNumber,Projectstage,out TotalCount);
+            ProjectAddResponse response = new ProjectAddResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 修改项目
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateProject(ProjectUpdateRequest request)
+        {
+            ProjectUpdateResponse response = new ProjectUpdateResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 分页显示所有项目
+        /// </summary>
+        /// <returns></returns>
+        public ProjectGetResponse ProjectShow(ProjectGetRequest request)
+        {
+            ProjectGetResponse response = new ProjectGetResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 查询项目详情
+        /// </summary>
+        /// <returns></returns>
+        public ProjectGetResponse ProjectDetail(ProjectGetRequest request)
+        {
+            ProjectGetResponse response = new ProjectGetResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 项目阶段详情
+        /// </summary>
+        /// <param name="ProjectStageId"></param>
+        /// <returns></returns>
+        public ProjectGetResponse StageDetail(ProjectGetRequest request)
+        {
+            ProjectGetResponse response = new ProjectGetResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 新增阶段
+        /// </summary>
+        /// <returns></returns>
+        public ProjectAddResponse AddStage(ProjectAddRequest request)
+        {
+            ProjectAddResponse response = new ProjectAddResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 逻辑删除修改阶段
+        /// </summary>
+        public ProjectDeleteResponse DeleteStage(ProjectDeleteRequest request)
+        {
+            ProjectDeleteResponse response = new ProjectDeleteResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 修改阶段
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateStage(ProjectUpdateRequest request)
+        {
+            ProjectUpdateResponse response = new ProjectUpdateResponse();
+
+            return response; 
+        }
+
+        /// <summary>
+        /// 修改项目状态
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateProjectStaus(ProjectUpdateRequest request)
+        {
+            ProjectUpdateResponse response = new ProjectUpdateResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 绑定项目状态
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse BandProjectStaus(ProjectUpdateRequest request)
+        {
+            ProjectUpdateResponse response = new ProjectUpdateResponse();
+
+            return response;
+        }
+
+        /// <summary>
+        /// 逻辑删除项目
+        /// </summary>
+        /// <returns></returns>
+        public ProjectDeleteResponse UpdateStausProject(ProjectDeleteRequest request)
+        {
+            ProjectDeleteResponse response = new ProjectDeleteResponse();
+
+            return response;
         }
     }
 }
