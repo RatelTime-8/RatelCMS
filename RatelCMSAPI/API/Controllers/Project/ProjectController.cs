04ea6f26f4e41d3129e60f6a9a483d3391b0a282
ASUS <ASUS@DESKTOP-300DDIH>
2020-03-25 10:55:55 +0800

    Bll层封装

--- RatelCMSAPI/API/Controllers/Project/ProjectController.cs
+++ RatelCMSAPI/API/Controllers/Project/ProjectController.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using BLL;
-using Model;
 using SDKClient;
 
 namespace API.Controllers
@@ -40,6 +39,96 @@ namespace API.Controllers
         public ProjectUpdateResponse UpdateProject(ProjectUpdateRequest request)
         {
             return bll.UpdateProject(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 分页显示所有项目
+        /// </summary>
+        /// <returns></returns>
+        public ProjectGetResponse ProjectShow(ProjectGetRequest request)
+        {
+            return bll.ProjectShow(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 查询项目详情
+        /// </summary>
+        /// <returns></returns>
+        public ProjectGetResponse ProjectDetail(ProjectGetRequest request)
+        {
+            return bll.ProjectDetail(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 项目阶段详情
+        /// </summary>
+        /// <param name="ProjectStageId"></param>
+        /// <returns></returns>
+        public ProjectGetResponse StageDetail(ProjectGetRequest request)
+        {
+            return bll.StageDetail(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 新增阶段
+        /// </summary>
+        /// <returns></returns>
+        public ProjectAddResponse AddStage(ProjectAddRequest request)
+        {
+            return bll.AddStage(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 逻辑删除修改阶段
+        /// </summary>
+        public ProjectUpdateResponse DeleteStage(ProjectUpdateRequest request)
+        {
+            return bll.DeleteStage(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 修改阶段
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateStage(ProjectUpdateRequest request)
+        {
+            return bll.UpdateStage(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 修改项目状态
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateProjectStaus(ProjectUpdateRequest request)
+        {
+            return bll.UpdateProjectStaus(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 绑定项目状态
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse BandProjectStaus(ProjectUpdateRequest request)
+        {
+            return bll.BandProjectStaus(request);
+        }
+
+        [HttpPost]
+        /// <summary>
+        /// 逻辑删除项目
+        /// </summary>
+        /// <returns></returns>
+        public ProjectUpdateResponse UpdateStausProject(ProjectUpdateRequest request)
+        {
+            return bll.UpdateStausProject(request);
         }
     }
 }
